8f2f343542ac6c47466c73211409d2e810e4dd24
almartson <6304456+almartson@users.noreply.github.com>
2020-01-29 16:16:26 -0500

    1- First commit (code). 2- Adding all Assets.

---
+++ Assets/Scripts/ClickToLoadAsyncCoroutine.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickToLoadAsyncCoroutine : MonoBehaviour
+{
+
+    /// <summary>
+    /// My loading (Slider - Progress) Bar.
+    /// </summary>
+    [Tooltip("Loading (Slider - Progress) Bar")]
+    public Slider _myLoadingBar;
+
+    /// <summary>
+    /// My loading image.
+    /// </summary>
+    [Tooltip("Image with Slider and al UI stuff that will appear: while Loading.")]
+    public GameObject _myLoadingImage;
+
+    /// <summary>
+    /// Async operation object, which will allow for 'asking questions', such as: 'Are you done?'.
+    /// </summary>
+    private AsyncOperation _myAsyncOperation;
+
+
+    #region Custom Methods
+
+    public void ClickAsync(int level)
+    {
+        _myLoadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }//End Method
+
+
+    IEnumerator LoadLevelWithBar (int level)
+    {
+        _myAsyncOperation = Application.LoadLevelAsync(level);
+        while (!_myAsyncOperation.isDone)
+        {
+            _myLoadingBar.value = _myAsyncOperation.progress;
+            yield return null;
+        }
+    }//End Method
+
+    #endregion Custom Methods
+
+}
---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class allows us to load a scene when clicked.
+/// </summary>
+public class LoadOnClick : MonoBehaviour
+{
+
+    /// <summary>
+    /// My loading image.
+    /// </summary>
+    [Tooltip("Image with Slider and al UI stuff that will appear: while Loading.")]
+    public GameObject _myLoadingImage;
+
+
+    public void LoadScene(int level)
+    {
+        // Set the image as: Active.
+        //
+        if ( this._myLoadingImage != null )
+        {
+            this._myLoadingImage.SetActive(true);
+
+        }//End if
+        //
+        Application.LoadLevel(level);
+    }
+}
